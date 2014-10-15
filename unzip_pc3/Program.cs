ba5b72383db3a6f485dedc5ce869e137d15aeb15
Boxa <Boxa.shu@gmail.com>
2014-10-15 12:24:19 +0400

    добавил заготовку упаковщика

--- unzip_pc3/Program.cs
+++ unzip_pc3/Program.cs
@@ -12,16 +12,23 @@ namespace unzip_pc3
 {
     class Program
     {
+        // args[0] - ключ для паковки / распаковки
+        //              e - распаковать / extract
+        //              c - запаковать / compress
+        // args[1] - путь до файла, который будем паковать или распаковывать / path to file
+        
         static void Main(string[] args)
         {
+            //' на основе вот этого топака : http://www.theswamp.org/index.php?topic=41529.0
 
             if(args.Length == 2){
+                // тут распаковываем архивчик
                 if (args[0] == "e")
                 {
                     if(File.Exists(args[1]))
                     {
-                        //' на основе вот этого топака : http://www.theswamp.org/index.php?topic=41529.0
-                        string fileName =  args[0]; // "E:\\3\\Default Windows System Printer.pc3";
+                        
+                        string fileName =  args[1];
                         using (FileStream fs = File.Open(fileName, FileMode.Open, FileAccess.Read))
                         {
                             fs.Seek(60L, SeekOrigin.Begin);
@@ -41,17 +48,46 @@ namespace unzip_pc3
                                 }
                             }
                         }
-
-
                     }
                 }
 
+                // Тут запаковываем все обратно
                 if (args[0] == "c")
                 {
+                    if (File.Exists(args[1]))
+                    {
+                        string fileName = args[1];
+                        using (FileStream fs = File.Open(fileName, FileMode.Open, FileAccess.Read))
+                        {
+                            using (StreamReader sr = new StreamReader(fs))
+                            {
+                                String pref_s = "PIAFILEVERSION_2.0,PC3VER1,compress\r\npmzlibcodec";
+                                
+                                //Byte[] pref_byte = { 255,255,255,255,255,255,255,000,255,255,255,000 };
+                                //Byte[] pref_byte = { 157, 94, 173, 006, 215, 016, 000, 000, 199, 004, 000, 000 };
+                                // Эти данные(байты) должны вычеслятся
+                                Byte[] pref_byte = { 157, 94, 173, 006, 215, 016, 000, 000, 199, 004, 000, 000 };
 
+                                pref_s = pref_s + Encoding.Default.GetString(pref_byte);
+
+                                String s = sr.ReadToEnd();
+                                                               
+
+                                using (FileStream fs_out = File.Open(fileName + ".pc3", FileMode.Create, FileAccess.ReadWrite))
+                                {
+                                    using (ZlibStream zs = new ZlibStream(fs_out, CompressionMode.Compress,
+                                                                             CompressionLevel.BestCompression, false))
+                                    {
+                                        fs_out.Write(Encoding.Default.GetBytes(pref_s), 0, Encoding.Default.GetBytes(pref_s).Length);
+                                        zs.Write(Encoding.Default.GetBytes(s), 0, Encoding.Default.GetBytes(s).Length);
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
              }
-         //Console.ReadLine();
+
 
         }
     }

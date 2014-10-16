ff11b5f179eb4dbad7bfccc180672dedda62e2ce
Boxa <Boxa.shu@gmail.com>
2014-10-16 09:54:33 +0400

    Переписал расчет размера запакованных данны�

--- unzip_pc3/Program.cs
+++ unzip_pc3/Program.cs
@@ -20,16 +20,13 @@ namespace unzip_pc3
         static void Main(string[] args)
         {
             //' на основе вот этого топака : http://www.theswamp.org/index.php?topic=41529.0
-
-
             if(args.Length == 2){
-                // тут распаковываем архивчик
-                if (args[0] == "e")
+                if (File.Exists(args[1]))
                 {
-                    if(File.Exists(args[1]))
+                    // тут распаковываем архивчик
+                    if (args[0] == "e")
                     {
-                        
-                        string fileName =  args[1];
+                        string fileName = args[1];
                         using (FileStream fs = File.Open(fileName, FileMode.Open, FileAccess.Read))
                         {
                             fs.Seek(60L, SeekOrigin.Begin);
@@ -43,34 +40,23 @@ namespace unzip_pc3
 
                                     using (FileStream fs_out = File.Open(fileName + ".txt", FileMode.Create, FileAccess.ReadWrite))
                                     {
-                                       
                                         fs_out.Write(Encoding.Default.GetBytes(s), 0, Encoding.Default.GetBytes(s).Length);
                                     }
-
-                                    //using (StreamWriter fs_out = File.CreateText(fileName + ".txt"))
-                                    //{
-                                       
-                                    //    fs_out.WriteLine(s);
-
-                                    //}
                                 }
                             }
                         }
                     }
-                }
 
-                // Тут запаковываем все обратно
-                if (args[0] == "c")
-                {
-                    if (File.Exists(args[1]))
+                    // Тут запаковываем все обратно
+                    if (args[0] == "c")
                     {
                         string fileName = args[1];
                         using (FileStream fs = File.Open(fileName, FileMode.Open, FileAccess.Read))
                         {
                             using (StreamReader sr = new StreamReader(fs))
                             {
+                                // заголовок файла, без него автокад не работает
                                 String pref_s = "PIAFILEVERSION_2.0,PC3VER1,compress\r\npmzlibcodec";
-
                                 /*
                                 is a good starting point, but contains several errors/lack of information
                                 The header size is 60 No 59
@@ -79,33 +65,33 @@ namespace unzip_pc3
                                 bytes 57->60 = compressed stream size
                                 Эти данные(байты) должны вычеслятся
                                 */
-                                Byte[] ZlibCodec_Adler32 = { 157, 94, 173, 006 };
-
-                                long decompresse_stream_size = fs.Length;
-                                long compressed_stream_size = fs.Length;
-                                
-
                                 String s = sr.ReadToEnd();
-                                                               
+                                //Byte[] ZlibCodec_Adler32 = { 157, 94, 173, 006 };
+                                long decompresse_stream_size = fs.Length;
+                                //тут считаем размер запакованных данных
+                                byte[] compressed_stream_size;
+                                using (var ms = new MemoryStream())
+                                {
+                                    using (ZlibStream deflateStream = new ZlibStream(ms, CompressionMode.Compress,
+                                                                             CompressionLevel.BestCompression, false))
+                                    {
+                                     deflateStream.Write(Encoding.Default.GetBytes(s), 0, Encoding.Default.GetBytes(s).Length);
+                                    }
+                                    compressed_stream_size = ms.ToArray();
+                                }
 
+                                // Тут все пишем в файл
                                 using (FileStream fs_out = File.Open(fileName + ".pc3", FileMode.Create, FileAccess.ReadWrite))
                                 {
-                                    
-                                    
                                     using (ZlibStream zs = new ZlibStream(fs_out, CompressionMode.Compress,
                                                                              CompressionLevel.BestCompression, false))
                                     {
-
-                                        // тут надо получить размер запакованных данных и сохранить в compressed_stream_size
-                                        //но как это сделать пока не знаю
-
                                         fs_out.Write(Encoding.Default.GetBytes(pref_s), 0, Encoding.Default.GetBytes(pref_s).Length);
-                                        fs_out.Write(ZlibCodec_Adler32, 0, 4);
+                                        //fs_out.Write(ZlibCodec_Adler32, 0, 4);
+                                        fs_out.Write(BitConverter.GetBytes(new ZlibCodec(CompressionMode.Compress).Adler32), 0, 4);
                                         fs_out.Write(BitConverter.GetBytes(decompresse_stream_size), 0, 4);
-                                        fs_out.Write(BitConverter.GetBytes(compressed_stream_size), 0, 4);
-
+                                        fs_out.Write(BitConverter.GetBytes(compressed_stream_size.Length), 0, 4);
                                         zs.Write(Encoding.Default.GetBytes(s), 0, Encoding.Default.GetBytes(s).Length);
-                                        
                                     }
                                 }
                             }
@@ -113,8 +99,6 @@ namespace unzip_pc3
                     }
                 }
              }
-
-
         }
     }
 }
